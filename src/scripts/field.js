9234672740ba8d09a07fbb12f555b201a9aa403f
ted <tedmarov@gmail.com>
2020-10-08 21:24:02 -0500

    Added plants, fields, and isarray

---
+++ src/scripts/field.js
@@ -0,0 +1,14 @@
+const fieldArray = [
+    
+]
+
+export const addPlant = (seedObj) => {
+    if (Array.isArray(seedObj) == true)
+        fieldArray.push(seedObj)
+    else
+        fieldArray.push(seedObj) 
+}
+
+export const usePlants = () => {
+    return fieldArray.slice()
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
 import { createCorn } from "./seeds/corn.js"
+import { addPlant } from "./field.js"
+import { usePlants } from "./field.js"
 
 const cornSeed = createCorn()
 
-console.log(cornSeed)
+const cornPlant = addPlant(cornSeed)
+const cornField = usePlants(cornPlant)
+
+console.log(cornField)
