2c11355751f4577dd86e7248228b601b89a5a33a
Ted Marov <ted.marov@gmail.com>
2021-07-08 23:03:12 -0500

    Keeping changes on branch

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,18 +2,6 @@
 export const harvestPlants = (allPlanted) => {
     const plantsToBeSold = []
 
-    allPlanted.forEach(plant => {
-        let output = plant.output
-
-        if (plant.type === "Corn") {
-            output /= 2
-        }
-
-        for (let i = 0; i < output; i++) {
-         plantsToBeSold.push(plant)
-    }
-
-    })
     return plantsToBeSold
 }
 //     for (let i = 0; i < seedArray.length; i++)
--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,42 +1,99 @@
+import { addPlant } from "./field.js"
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from "./seeds/corn.js"
 import { createPotato } from "./seeds/potato.js"
 import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createWheat } from "./seeds/wheat.js"
-import { addPlant } from "./field.js"
 
-export const plantSeeds = (plan) => {
-    plan.forEach(row => {
-        row.forEach(plantType => {
-            switch (plantType) {
-                case "Asparagus":
-                    addPlant(createAsparagus())
-                    break
-                case "Corn":
-                    addPlant(createCorn())
-                    break
-                case "Potato":
-                    addPlant(createPotato())
-                    break
-                case "Soybean":
-                    addPlant(createSoybean())
-                    break
-                case "Sunflower":
-                    addPlant(createSunflower())
-                    break
-                case "Wheat":
-                    addPlant(createWheat())
-                    break
-                default:
-                    break
+//Stuck on sowing, npm run test was not returning right value
 
-            }
-            console.log(plantType)
-        })
-
-})
+export const plantSeeds = (plantingPlan) =>  {
+   for (let i = 0; i < plantingPlan.length; i++) {
+        for (let j = 0; j < plantingPlan[i].length; j++) {
+        if (plantingPlan[i][j] === "Asparagus")
+        {addPlant(createAsparagus())} 
+        else if (plantingPlan[i][j] === "Corn") {
+        let plantedSeed = createCorn()
+        addPlant(plantedSeed[0])
+        addPlant(plantedSeed[1])}
+        else if (plantingPlan[i][j] === "Potato")
+        {addPlant(createPotato())} 
+        else if (plantingPlan[i][j] === "Soybean")
+        {addPlant(createSoybean())}
+        else if (plantingPlan[i][j] === "Sunflower")
+        {addPlant(createSunflower())}
+        else if (plantingPlan[i][j] === "Wheat")
+        {addPlant(createWheat())}
+    }
 }
+return addPlant()
+}
+
+
+// for (let i = 0; i < plantingPlan.length; i++) {
+//     for (let j = 0; j < plantingPlan[i].length; j++) {
+//         let seedFinder = plantingPlan[i][j]
+//     if (seedFinder === "Asparagus") {
+//         let plantedSeed = createAsparagus()
+//         addPlant(plantedSeed) }        
+//     else if (seedFinder === "Corn") {
+//         let plantedSeed = createCorn() 
+//         addPlant(plantedSeed[0])
+//         addPlant(plantedSeed[1]) }
+//     else if (seedFinder === "Potato") {
+//         let plantedSeed = createPotato()
+//         addPlant(plantedSeed) }
+//     else if (seedFinder === "Soybean") {
+//         let plantedSeed = createSoybean()
+//         addPlant(plantedSeed) }
+//     else if (seedFinder === "Sunflower") {
+//         let plantedSeed = createSunflower()
+//         addPlant(plantedSeed) }
+//     else (seedFinder === "Wheat"); {
+//         let plantedSeed = createWheat()
+//         addPlant(plantedSeed) }
+//     } 
+//     console.log(plantingPlan)
+// return plantingPlan
+// }
+
+// export const plantSeeds = (plan) => {
+// plan.forEach(row => {
+// row.forEach(plantType => {
+//     switch (plantType) {
+//         case "Asparagus":
+//             addPlant(createAsparagus())
+//             break
+//         case "Corn":
+//             addPlant(createCorn())
+//             break
+//         case "Potato":
+//             addPlant(createPotato())
+//             break
+//         case "Soybean":
+//             addPlant(createSoybean())
+//             break
+//         case "Sunflower":
+//             addPlant(createSunflower())
+//             break
+//         case "Wheat":
+//             addPlant(createWheat())
+//             break
+//         default:
+//             break
+// }
+// })
+// })
+// }
+ 
+//             console.log(plantType)
+//         })
+
+// })
+// }
+
+
 
 /* Define and export plantSeeds
     Accept planting plan/define parameter as input */
@@ -50,35 +107,4 @@ export const plantSeeds = (plan) => {
 //     else if
 //     else if
 //     else if
-// }
-
-// export const plantSeeds = (plantingPlan) =>  {
-//     let plantedSeeds = {}
-//     for (let i = 0; i < plantingPlan.length; i++) {
-//         let subplantingPlan = plantingPlan[i];
-//         for (let j = 0; j < subplantingPlan.length; j++) {
-//         if (subplantingPlan[j] === "Asparagus") {
-//             plantedSeeds = createAsparagus()
-//             plantingPlan.concat(plantedSeeds); }
-//         else if (subplantingPlan[j] === "Corn") {
-//             plantedSeeds = createCorn() 
-//             addPlant(plantedSeeds); }
-//         else if (subplantingPlan[j] === "Potato") {
-//             plantedSeeds = createPotato()
-//             addPlant(plantedSeeds); }
-//         else if (subplantingPlan[j] === "Soybean") {
-//             plantedSeeds = createSoybean()
-//             addPlant(plantedSeeds); }
-//         else if (subplantingPlan[j] === "Sunflower") {
-//             plantedSeeds = createSunflower()
-//             addPlant(plantedSeeds); }
-//         else (subplantingPlan[j] === "Wheat"); {
-//             plantedSeeds = createWheat()
-//             addPlant(plantedSeeds) }
-//         } 
-        
-//     }
-// console.log(yearlyPlan)
-// return yearlyPlan
-
 // }
